6cc1779bb484510f75e07454508864f5250683f7
TroelsAgergaard <troelsagergaard@gmail.com>
2021-08-19 10:40:06 +0200

    Add solution to assignment 02a

--- eks_02a_funktioner/js/index.js
+++ eks_02a_funktioner/js/index.js
@@ -1,6 +1,9 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
+  const hilsen = "Hej med dig";
+  function hils() {
+    console.log(hilsen);
+  }
 
-	// skriv din kode her...
-
+  hils();
 }); // Afslutter: DOMContentLoaded
